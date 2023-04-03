2110b02266b09402597b1e680c8a1db51f21dd0f
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-04 01:27:34 +0800

    [controller] create expense controller

---
+++ backend/src/controllers/expense_controller.js
@@ -0,0 +1,52 @@
+import { Expense } from "../models/expense_model.js";
+
+const getExpensesByGroupId = async (gid) => {
+    try {
+        return await Expense.find({ attached_group_id: gid });
+    } catch (error) {
+        return -1;
+    }
+};
+
+const getExpensesByExpenseId = async (eid) => {
+    try {
+        return await Expense.find({ expense_id: eid });
+    } catch (error) {
+        return -1;
+    }
+};
+
+const createExpense = async (expenseObject) => {
+    try {
+        const newExpense = new Expense(expenseObject);
+        return await newExpense.save();
+    } catch (error) {
+        console.error(error);
+        return { _id: -1 };
+    }
+};
+
+const getGroupExpenses = async (req, res, next) => {
+    const groupId = req.query.gid;
+    const expenses = await getExpensesByGroupId(groupId);
+    if (expenses === -1) {
+        return res.status(500).json({ msg: "Internal Server Error" });
+    } else {
+        return res.status(200).json({ data: expenses });
+    }
+};
+
+const createGroupExpense = async (req, res, next) => {
+    const expenseObject = req.body;
+    console.log("body", expenseObject);
+    const { _id } = await createExpense(expenseObject);
+    if (_id === -1) {
+        return res.status(500).json({ msg: "Internal Server Error" });
+    } else {
+        return res
+            .status(200)
+            .json({ msg: `New expense_id: ${_id} was created!!` });
+    }
+};
+
+export { getGroupExpenses, createGroupExpense };
