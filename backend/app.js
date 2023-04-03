bbb38acb6980de1ec2518b3266f714828b18735a
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-04 01:26:50 +0800

    [express] add expense router

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,24 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import logger from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { API_VERSION } = process.env;
 
 import indexRouter from "./src/routes/index.js";
 import usersRouter from "./src/routes/users.js";
+import expenseRouter from "./src/routes/expense_route.js";
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// API routes
+app.use("/api/" + API_VERSION, [indexRouter, usersRouter, expenseRouter]);
 
 app.use(logger("dev"));
 app.use(express.json());
