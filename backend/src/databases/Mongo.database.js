264c5d8f12745bed7ac7e0cd72db6c34da897665
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-04 01:25:41 +0800

    [database] update Mongo.database.js

--- backend/src/databases/Mongo.database.js
+++ backend/src/databases/Mongo.database.js
@@ -11,34 +11,4 @@ mongoose.connection.on("error", (error) =>
     console.error("database error", error)
 );
 
-const ExpenseSchema = new mongoose.Schema({
-    expense_id: { type: String, required: true, unique: true },
-    description: { type: String },
-    cost: {
-        type: Number,
-    },
-    split_method: {
-        type: String,
-        enum: [
-            "split equally",
-            "split by exact amounts",
-            "split by percentages",
-            "split by shares",
-            "split by adjustment",
-        ],
-        required: true,
-    },
-    attached_group_id: { type: Number, default: null }, // index key
-    involved_users: {
-        type: Map,
-        of: Number, // MAP<user_id:balance>
-        // People who will receive money from others. They have a positive balance.
-        // People who will pay money to others. They have a negative balance.
-    },
-    comments: {},
-    date: { type: Date, default: Date.now },
-    createTime: {
-        type: Date,
-        default: Date.now,
-    },
-});
+export { mongoose };
