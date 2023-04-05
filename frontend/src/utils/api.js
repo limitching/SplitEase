1c2c4050f767733b35fc97783113baea6c2cec37
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-05 20:22:28 +0800

    [frontend]create expense API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,12 +4,18 @@ const HOST = "http://localhost:3000";
 const api = {
     hostname: HOST + "/api/1.0",
     getMembers: async function (gid) {
-        const { data } = await axios(`${this.hostname}/group/${gid}/members`);
+        const { data } = await axios.get(
+            `${this.hostname}/group/${gid}/members`
+        );
         return data;
     },
     getCurrencies: async function () {
-        const { data } = await axios(`${this.hostname}/currencies`);
+        const { data } = await axios.get(`${this.hostname}/currencies`);
         return data;
+    },
+    createExpense: async function (data) {
+        const result = await axios.post(`${this.hostname}/expense`, data);
+        return result;
     },
 };
 export { api };
