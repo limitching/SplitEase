27ac1f4e821f4f9c4b9aeaa9fd02e8647b0d984c
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-06 18:20:18 +0800

    [constant] add CURRENCY_OPTIONS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
--- frontend/src/global/constant.js
+++ frontend/src/global/constant.js
@@ -5,4 +5,56 @@ const SPLIT_METHODS = [
     "split by shares",
     "split by adjustment",
 ];
-export { SPLIT_METHODS };
+const CURRENCY_OPTIONS = [
+    {
+        id: 1,
+        abbreviation: "TWD",
+        symbol: "NT$",
+    },
+    {
+        id: 2,
+        abbreviation: "USD",
+        symbol: "$",
+    },
+    {
+        id: 3,
+        abbreviation: "EUR",
+        symbol: "€",
+    },
+    {
+        id: 4,
+        abbreviation: "JPY",
+        symbol: "¥",
+    },
+    {
+        id: 5,
+        abbreviation: "GBP",
+        symbol: "£",
+    },
+    {
+        id: 6,
+        abbreviation: "AUD",
+        symbol: "A$",
+    },
+    {
+        id: 7,
+        abbreviation: "CAD",
+        symbol: "C$",
+    },
+    {
+        id: 8,
+        abbreviation: "CHF",
+        symbol: "CHF",
+    },
+    {
+        id: 9,
+        abbreviation: "CNY",
+        symbol: "CN¥",
+    },
+    {
+        id: 10,
+        abbreviation: "HKD",
+        symbol: "HK$",
+    },
+];
+export { SPLIT_METHODS, CURRENCY_OPTIONS };
