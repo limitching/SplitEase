1bb0e9a54144c4c44d2e2a985c7748082d7c0e56
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-09 14:30:13 +0800

    [model] add function to buildLevelGraph

--- backend/src/models/debts_model.js
+++ backend/src/models/debts_model.js
@@ -13,4 +13,29 @@ function buildResidualGraph(graph) {
     return residualGraph;
 }
 
-export { buildResidualGraph };
+// A function to build levelGraph
+function buildLevelGraph(residualGraph, start, end) {
+    const N = residualGraph.length;
+    const levelGraph = new Array(N).fill(-1);
+    const queue = [];
+    queue.push(start);
+    levelGraph[start] = 0;
+
+    while (queue.length > 0) {
+        const u = queue.shift();
+        for (let v = 0; v < N; v++) {
+            if (residualGraph[u][v] > 0 && levelGraph[v] === -1) {
+                levelGraph[v] = levelGraph[u] + 1;
+                queue.push(v);
+            }
+        }
+    }
+
+    if (levelGraph[end] === -1) {
+        return null;
+    } else {
+        return levelGraph;
+    }
+}
+
+export { buildResidualGraph, buildLevelGraph };
