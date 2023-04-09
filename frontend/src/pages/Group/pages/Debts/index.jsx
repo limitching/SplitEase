5bc2e71c105800c9282f4e7b0956c8a9a843b99e
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-09 18:02:22 +0800

    [frontend] refactor

--- frontend/src/pages/Group/pages/Debts/index.jsx
+++ frontend/src/pages/Group/pages/Debts/index.jsx
@@ -2,22 +2,12 @@ import DebtsBlock from "./components/DebtsBlock";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../../../utils/api";
-import { Container, Row, Col } from "react-bootstrap";
-import { CURRENCY_OPTIONS } from "../../../../global/constant";
+import { Container, Row } from "react-bootstrap";
 
 async function fetchMembers(gid, setMembers) {
     try {
         const data = await api.getMembers(gid);
         setMembers(data);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function fetchGroupExpenses(gid, setGroupExpense) {
-    try {
-        const data = await api.getGroupExpenses(gid);
-        setGroupExpense(data);
     } catch (error) {
         console.error(error);
     }
@@ -34,43 +24,21 @@ async function fetchGroupDebts(gid, setDebts) {
 
 const Debts = () => {
     const { gid } = useParams();
-    const timeZoneOffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
-    const localISOTime = new Date(Date.now() - timeZoneOffset)
-        .toISOString()
-        .substring(0, 16);
 
     // States
     const [members, setMembers] = useState([]);
-    const [groupExpense, setGroupExpense] = useState([]);
-    const currencies = CURRENCY_OPTIONS;
     // const [currencies, setCurrencies] = useState([]);
-    const [checked, setChecked] = useState([]);
-    const [expenseTime, setExpenseTime] = useState(localISOTime);
+
     const [expensesChanged, setExpensesChanged] = useState(false);
-    const [selectedExpense, setSelectedExpense] = useState(null);
-    const [showTransaction, setShowTransaction] = useState(false);
-    const [showModification, setShowModification] = useState(false);
-    const [amount, setAmount] = useState(0);
-    const [selectedCurrency, setSelectedCurrency] = useState(1);
-    const [selectedCreditor, setSelectedCreditor] = useState(0);
-    const [selectedSplitMethod, setSelectedSplitMethod] = useState(0);
-    const [description, setDescription] = useState("");
     const [debts, setDebts] = useState([]);
 
     useEffect(() => {
         fetchMembers(gid, setMembers);
-        fetchGroupExpenses(gid, setGroupExpense);
         fetchGroupDebts(gid, setDebts);
     }, []);
 
     useEffect(() => {
-        if (members !== null) {
-            setChecked([...members]);
-        }
-    }, [members]);
-
-    useEffect(() => {
-        fetchGroupExpenses(gid, setGroupExpense);
+        fetchGroupDebts(gid, setDebts);
         setExpensesChanged(false);
     }, [expensesChanged]);
 
