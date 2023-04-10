538666ad2462f7d98ba3a599d4343d911abc7706
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-10 15:18:43 +0800

    [frontend] modified group layout

--- frontend/src/pages/Group/index.jsx
+++ frontend/src/pages/Group/index.jsx
@@ -1,63 +1,23 @@
 import GroupDashboard from "./components/Dashboard";
 import Tabs from "./components/Tabs";
-import {
-    GroupContextProvider,
-    GroupContext,
-} from "../../contexts/GroupContext";
-import { useState, useEffect, useContext } from "react";
-import { api } from "../../utils/api";
+import { GroupContextProvider } from "../../contexts/GroupContext";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
-async function fetchCurrencies(setCurrencies) {
-    try {
-        const data = await api.getCurrencies();
-        setCurrencies(data);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function fetchGroupExpenses(gid, setGroupExpense) {
-    try {
-        const data = await api.getGroupExpenses(gid);
-        setGroupExpense(data);
-    } catch (error) {
-        console.error(error);
-    }
-}
+const WrapperGroupContainer = styled.div`
+    padding-top: 5vh;
+    height: 100vh;
+    padding-bottom: 5vh;
+`;
 
 const Group = () => {
-    const { members, setMembers, gid } = useContext(GroupContext);
-    const timeZoneOffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
-    const localISOTime = new Date(Date.now() - timeZoneOffset)
-        .toISOString()
-        .substring(0, 16);
-
-    const [groupExpense, setGroupExpense] = useState([]);
-    const [currencies, setCurrencies] = useState([]);
-    const [checked, setChecked] = useState([]);
-    const [expenseTime, setExpenseTime] = useState(localISOTime);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        setIsLoading(true);
-        fetchGroupExpenses(gid, setGroupExpense);
-        setIsLoading(false);
-    }, [gid]);
-
-    useEffect(() => {
-        if (members !== null) {
-            setChecked([...members]);
-        }
-    }, [members]);
-
     return (
         <GroupContextProvider>
-            <div>
+            <WrapperGroupContainer>
                 <GroupDashboard></GroupDashboard>
                 <Tabs></Tabs>
                 <Outlet></Outlet>
-            </div>
+            </WrapperGroupContainer>
         </GroupContextProvider>
     );
 };
