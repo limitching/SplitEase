cde521a3c79d0c64c851376a7b0ad76945661d50
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-14 16:53:50 +0800

    [naming] unite naming rules

--- backend/src/routes/debts_route.js
+++ backend/src/routes/debts_route.js
@@ -4,6 +4,6 @@ import { wrapAsync } from "../utils/util.js";
 import { getGroupDebts } from "../controllers/debts_controller.js";
 
 /* GET debts data */
-router.route("/debts/:gid").get(wrapAsync(getGroupDebts));
+router.route("/debts/:group_id").get(wrapAsync(getGroupDebts));
 
 export default router;
