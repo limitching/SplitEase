adadd977b67f03f9ef376a0e418495b5d6008e0d
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-14 19:38:39 +0800

    [Login] add login page

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const WrapperGroupContainer = styled.div`
+    padding-top: 5vh;
+    height: 100vh;
+    padding-bottom: 5vh;
+`;
+
+const Login = () => {
+    return <WrapperGroupContainer></WrapperGroupContainer>;
+};
+
+export { Login };
