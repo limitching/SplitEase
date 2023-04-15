1bc211651b6cc5225ecd054f34d0ab04d2488896
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-15 14:38:48 +0800

    [route] add route for user signIn

--- backend/src/routes/user_route.js
+++ backend/src/routes/user_route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { signUp } from "../controllers/user_controller.js";
-import expenseValidator from "../middlewares/validators/signUpValidator.js";
+import { signUp, signIn } from "../controllers/user_controller.js";
+import signUpValidator from "../middlewares/validators/signUpValidator.js";
+import signInValidator from "../middlewares/validators/signInValidator.js";
 var router = express.Router();
 
 import { wrapAsync } from "../utils/util.js";
 
 /* User SignUp */
-router.route("/user/signup").post(expenseValidator, wrapAsync(signUp));
+router.route("/user/signup").post(signUpValidator, wrapAsync(signUp));
+router.route("/user/signin").post(signInValidator, wrapAsync(signIn));
 
 export default router;
