eeae05354c8ae2b97e11207becd68d08de33d174
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-15 14:39:52 +0800

    [controller] add logic for control signIn

--- backend/src/controllers/user_controller.js
+++ backend/src/controllers/user_controller.js
@@ -43,4 +43,59 @@ const signUp = async (req, res) => {
         },
     });
 };
-export { signUp };
+
+const signIn = async (req, res) => {
+    const data = req.body;
+    let result;
+    switch (data.provider) {
+        case "native":
+            result = await nativeSignIn(data.email, data.password);
+            break;
+        case "line":
+            break;
+        default:
+            result = { error: "Request Error: Wrong Request" };
+    }
+    console.log(result);
+    if (result.status === 400) {
+        return res.status(400).send({ error: result.error });
+    }
+    const user = result.user;
+    if (!user || result.status === 500) {
+        return res
+            .status(500)
+            .send({ error: "Server Error: Database Query Error" });
+    }
+
+    const accessToken = jwt.sign(
+        {
+            provider: user.provider,
+            name: user.name,
+            email: user.email,
+            image: user.picture,
+        },
+        TOKEN_SECRET,
+        { expiresIn: TOKEN_EXPIRE }
+    );
+
+    return res.status(200).json({
+        access_token: accessToken,
+        access_expired: TOKEN_EXPIRE,
+        login_at: user.login_at,
+        user: {
+            id: user.id,
+            provider: user.provider,
+            name: user.name,
+            email: user.email,
+        },
+    });
+};
+
+const nativeSignIn = async (email, password) => {
+    try {
+        return await User.nativeSignIn(email, password);
+    } catch (error) {
+        return { error };
+    }
+};
+export { signUp, signIn };
