fcf5f7db1ced347c3c24325f70a34e416a322b9e
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-15 14:38:25 +0800

    [middlewares] create signInValidator

---
+++ backend/src/middlewares/validators/signInValidator.js
@@ -0,0 +1,42 @@
+import { check, validationResult } from "express-validator";
+
+export default [
+    check("provider")
+        .exists()
+        .withMessage("Request Error: provider are required."),
+    check("email")
+        .if((value, { req }) => req.body.provider === "native")
+        .exists()
+        .withMessage("Request Error: name, email and password are required.")
+        .bail()
+        .notEmpty()
+        .withMessage("Request Error: name, email and password are required.")
+        .bail()
+        .isEmail()
+        .withMessage("Request Error: Invalid email format")
+        .bail(),
+    check("password")
+        .if((value, { req }) => req.body.provider === "native")
+        .exists()
+        .withMessage("Request Error: name, email and password are required.")
+        .isLength({ min: 4, max: 16 })
+        .withMessage("Request Error: password length should be between 8 - 16")
+        .custom((value) => {
+            const pwdregex =
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{4,16}$/;
+            if (!value.match(pwdregex)) {
+                throw new Error(
+                    "Request Error: password should be at least one lowercase, one uppercase, one digit, one special character"
+                );
+            } else {
+                return true;
+            }
+        })
+        .bail(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(400).json({ errors: errors.array() });
+        next();
+    },
+];
