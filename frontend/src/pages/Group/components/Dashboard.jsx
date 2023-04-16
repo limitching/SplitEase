0333b66c0719bba272b76e77205b3852b04809f5
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-17 02:17:39 +0800

    [Group] modify dashboard layout

--- frontend/src/pages/Group/components/Dashboard.jsx
+++ frontend/src/pages/Group/components/Dashboard.jsx
@@ -1,4 +1,7 @@
+import { useContext } from "react";
+import { GroupContext } from "../../../contexts/GroupContext";
 const GroupDashboard = () => {
+    const { group } = useContext(GroupContext);
     return (
         <div
             className="group-information"
@@ -8,10 +11,13 @@ const GroupDashboard = () => {
                 backgroundColor: "lightblue",
                 fontSize: "5rem",
                 display: "flex",
+                flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
             }}
         >
+            <h1>{group.name}</h1>
+            <h3>{group.description}</h3>
             Group Dashboard 預定地
         </div>
     );
