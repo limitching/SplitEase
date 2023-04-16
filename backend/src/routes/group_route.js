a36611ca6c17266940d6577e5756d21185cb64fa
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-16 20:41:44 +0800

    [route] add route to create group

--- backend/src/routes/group_route.js
+++ backend/src/routes/group_route.js
@@ -4,11 +4,15 @@ import { wrapAsync } from "../utils/util.js";
 import {
     getGroupInformation,
     getGroupMembers,
+    createNewGroup,
 } from "../controllers/group_controller.js";
+import { authentication } from "../utils/util.js";
 
 /* GET expense data */
 router.route("/group/:group_id").get(wrapAsync(getGroupInformation));
 
 router.route("/group/members/:group_id").get(wrapAsync(getGroupMembers));
+
+router.route("/group").post(authentication(), wrapAsync(createNewGroup));
 
 export default router;
