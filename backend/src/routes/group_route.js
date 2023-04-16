5b46e27ae96f35ac5b13fc8c7f35ec158bc71c67
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-16 21:01:57 +0800

    [route] import createGroupValidator

--- backend/src/routes/group_route.js
+++ backend/src/routes/group_route.js
@@ -7,12 +7,15 @@ import {
     createNewGroup,
 } from "../controllers/group_controller.js";
 import { authentication } from "../utils/util.js";
+import createGroupValidator from "../middlewares/validators/createGroupValidator.js";
 
 /* GET expense data */
 router.route("/group/:group_id").get(wrapAsync(getGroupInformation));
 
 router.route("/group/members/:group_id").get(wrapAsync(getGroupMembers));
 
-router.route("/group").post(authentication(), wrapAsync(createNewGroup));
+router
+    .route("/group")
+    .post(authentication(), createGroupValidator, wrapAsync(createNewGroup));
 
 export default router;
