49978a4865797a1a99e526ac0985904d728969e7
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-16 18:37:12 +0800

    [Home] modify CreateGroupModal

--- frontend/src/pages/home/components/CreateGroupModal.jsx
+++ frontend/src/pages/home/components/CreateGroupModal.jsx
@@ -1,62 +1,69 @@
-import { Container, Modal, Button, Form, Col, Row } from "react-bootstrap";
+import { Container, Modal, Button, Form, Row } from "react-bootstrap";
 import styled from "styled-components";
-import { TextField } from "@mui/material";
-import CurrencySelector from "../../Group/pages/Expenses/components/CurrencySelector";
+import { TextField, MenuItem } from "@mui/material";
+import { CURRENCY_OPTIONS } from "../../../global/constant";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { api } from "../../../utils/api";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
 
-const StyledExpenseImage = styled.img`
-    width: 400px;
-    hight: auto;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    overflow: scroll;
-`;
 const StyledModalBody = styled(Modal.Body)`
     max-height: 800px;
     overflow: scroll;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
 `;
-
-// const GroupDescription = () => {
-//     const { selectedExpense } = useContext(ExpenseContext);
-//     return (
-//         <Container className="expense-description mb-3">
-//             <Form.Label>Description</Form.Label>
-//             <Form.Control
-//                 as="textarea"
-//                 name="description"
-//                 rows={3}
-//                 defaultValue={
-//                     selectedExpense ? selectedExpense.description : ""
-//                 }
-//             />
-//         </Container>
-//     );
-// };
-
-// const GroupImage = () => {
-//     const { selectedExpense } = useContext(ExpenseContext);
-//     return (
-//         <Container className="expense-image mb-3">
-//             <Form.Label>Receipt photo</Form.Label>
-//             {selectedExpense && selectedExpense.image ? (
-//                 <Container>
-//                     <StyledExpenseImage
-//                         src={`http://localhost:3000/assets/${selectedExpense.image}`}
-//                         alt="Ops, your network is unstable, so this image cannot load."
-//                     ></StyledExpenseImage>
-//                 </Container>
-//             ) : null}
-//             <Form.Control type="file" name="image" />
-//         </Container>
-//     );
-// };
-// const ModalContent = () => {
-//     return <div></div>;
-// };
 
 const CreateGroupModal = ({
     showCreateGroupModal,
     handleCloseCreateGroupModal,
 }) => {
+    const { user, jwtToken } = useContext(AuthContext);
+    const [newGroupData, setNewGroupData] = useState({
+        owner: user.id,
+        name: "",
+        default_currency: 1,
+        description: "",
+    });
+    console.log(newGroupData);
+
+    function handleNewGroupDataChange(event) {
+        const key = event.target.name;
+        setNewGroupData({ ...newGroupData, [key]: event.target.value });
+    }
+
+    const createGroup = (event) => {
+        event.preventDefault();
+        const errors = [];
+        if (!newGroupData.owner) {
+            errors.push("Owner cannot be null");
+            errors.push("Please re-login again");
+        }
+        if (newGroupData.name.length === 0) {
+            errors.push("Group Name cannot be null");
+        }
+        if (errors.length !== 0) {
+            return MySwal.fire({
+                title: <p>Request Error</p>,
+                html: (
+                    <div>
+                        {errors.map((error) => (
+                            <p>{error}</p>
+                        ))}
+                    </div>
+                ),
+                icon: "error",
+                timer: 2000,
+                didOpen: () => {
+                    MySwal.showLoading();
+                },
+            });
+        }
+    };
+
     return (
         <Modal
             show={showCreateGroupModal}
@@ -66,24 +73,64 @@ const CreateGroupModal = ({
             size="md"
             centered
         >
-            <Form>
+            <Form onSubmit={createGroup}>
                 <Modal.Header closeButton as={Row}>
                     <Container className="modal-header-text ml-0 pl-0">
                         <h3>Create a new group</h3>
                     </Container>
                 </Modal.Header>
                 <StyledModalBody>
-                    <TextField
-                        name="name"
-                        className="mb-3"
-                        label="Group name"
-                        type="text"
-                    />
-                    <CurrencySelector></CurrencySelector>
+                    <Container>
+                        <TextField
+                            name="name"
+                            className="mb-3"
+                            label="Group name"
+                            type="text"
+                            value={newGroupData.name}
+                            onChange={handleNewGroupDataChange}
+                            variant="standard"
+                            fullWidth
+                            required
+                        />
+                    </Container>
+                    <Container>
+                        <TextField
+                            name="default_currency"
+                            select
+                            label="Currency for group debts"
+                            value={newGroupData.default_currency}
+                            onChange={handleNewGroupDataChange}
+                            variant="standard"
+                            fullWidth
+                            required
+                        >
+                            {CURRENCY_OPTIONS.map((option) => (
+                                <MenuItem key={option.id} value={option.id}>
+                                    {`${option.abbreviation}  (${option.symbol})`}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </Container>
+                    <Container>
+                        <TextField
+                            name="description"
+                            className="mb-3"
+                            label="Group description"
+                            type="text"
+                            variant="standard"
+                            value={newGroupData.description}
+                            onChange={handleNewGroupDataChange}
+                            fullWidth
+                            multiline
+                            rows={3}
+                        />
+                    </Container>
                 </StyledModalBody>
                 <Modal.Footer>
                     <Container className="d-grid">
-                        <Button variant="warning">Create a new group</Button>
+                        <Button variant="warning" type="submit">
+                            Create a new group
+                        </Button>
                     </Container>
                 </Modal.Footer>
             </Form>
