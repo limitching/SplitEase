1d85200d53e3fbc709f6e568f53bc58d00b4ec9d
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-16 17:38:28 +0800

    [Home] initiate home page after login

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import GroupsContainer from "./components/GroupsContainer";
+
+const HomeContainer = styled.div`
+    width: 100%;
+    padding-top: 5vh;
+    height: 100vh;
+    padding-bottom: 5vh;
+    background-color: skyblue;
+    margin: 0 auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
 const Home = () => {
-    return <div></div>;
+    return (
+        <HomeContainer>
+            <GroupsContainer></GroupsContainer>
+        </HomeContainer>
+    );
 };
 
 export default Home;
