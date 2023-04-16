2abe2995afdbf4ce63f76d5542382cee0dd423db
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-16 15:50:41 +0800

    [route] add route to get user group

--- backend/src/routes/user_route.js
+++ backend/src/routes/user_route.js
@@ -1,13 +1,20 @@
 import express from "express";
-import { signUp, signIn } from "../controllers/user_controller.js";
+import {
+    signUp,
+    signIn,
+    getGroups,
+    getUserProfile,
+} from "../controllers/user_controller.js";
 import signUpValidator from "../middlewares/validators/signUpValidator.js";
 import signInValidator from "../middlewares/validators/signInValidator.js";
 var router = express.Router();
 
-import { wrapAsync } from "../utils/util.js";
+import { wrapAsync, authentication } from "../utils/util.js";
 
 /* User SignUp */
 router.route("/user/signup").post(signUpValidator, wrapAsync(signUp));
 router.route("/user/signin").post(signInValidator, wrapAsync(signIn));
+router.route("/user/groups").get(authentication(), wrapAsync(getGroups));
+router.route("/user/profile").get(authentication(), wrapAsync(getUserProfile));
 
 export default router;
