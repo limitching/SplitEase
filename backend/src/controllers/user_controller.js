1e025b66cd6ad1f397830dff21b6653b03fc1596
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-17 19:38:08 +0800

    [fix] fix user Profile response

--- backend/src/controllers/user_controller.js
+++ backend/src/controllers/user_controller.js
@@ -139,7 +139,7 @@ const getUserProfile = async (req, res) => {
     return res.status(200).json({
         id: req.user.id,
         provider: req.user.provider,
-        email: eq.user.email,
+        email: req.user.email,
         name: req.user.name,
         image: req.user.image,
     });
