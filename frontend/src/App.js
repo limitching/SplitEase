2b9894f8498e42121976b2bc7943ef6b8084d273
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-17 18:06:06 +0800

    [refactor] modify import header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { GroupContextProvider } from "./contexts/GroupContext";
 
 function App() {
     return (
         <>
             <AuthContextProvider>
-                <Header></Header>
-                <Outlet />
-                <Footer></Footer>
+                <GroupContextProvider>
+                    <Header></Header>
+                    <Outlet />
+                    <Footer></Footer>
+                </GroupContextProvider>
             </AuthContextProvider>
         </>
     );
