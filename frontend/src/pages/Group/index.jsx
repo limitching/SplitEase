9acc9bb3c67575f5277dfea10f4f93f507b65f2e
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-17 15:23:43 +0800

    [Group] add logic to render error when no access right

--- frontend/src/pages/Group/index.jsx
+++ frontend/src/pages/Group/index.jsx
@@ -1,9 +1,13 @@
 import GroupDashboard from "./components/Dashboard";
 import Tabs from "./components/Tabs";
+import Error from "./components/Error";
 import { GroupContextProvider } from "../../contexts/GroupContext";
 import { ExpenseContextProvider } from "../../contexts/ExpenseContext";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { GroupContext } from "../../contexts/GroupContext";
 
 const WrapperGroupContainer = styled.div`
     padding-top: 5vh;
@@ -12,14 +16,27 @@ const WrapperGroupContainer = styled.div`
 `;
 
 const Group = () => {
+    const { isLogin } = useContext(AuthContext);
+    const { group } = useContext(GroupContext);
+    console.log("isLogin", isLogin);
+    const { loading } = useContext(AuthContext);
+    console.log("isLogin", isLogin);
+    console.log("loading", loading);
+    console.log("group", group);
     return (
         <GroupContextProvider>
             <ExpenseContextProvider>
-                <WrapperGroupContainer>
-                    <GroupDashboard></GroupDashboard>
-                    <Tabs></Tabs>
-                    <Outlet></Outlet>
-                </WrapperGroupContainer>
+                {isLogin || group.id ? (
+                    <WrapperGroupContainer>
+                        <GroupDashboard></GroupDashboard>
+                        <Tabs></Tabs>
+                        <Outlet></Outlet>
+                    </WrapperGroupContainer>
+                ) : (
+                    <WrapperGroupContainer>
+                        <Error></Error>
+                    </WrapperGroupContainer>
+                )}
             </ExpenseContextProvider>
         </GroupContextProvider>
     );
