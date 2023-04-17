472e77da01ec61c33eacf8a03d7f159783140920
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-17 10:49:12 +0800

    [controller] add 400 & 500 response for getPublicInformation

--- backend/src/controllers/group_controller.js
+++ backend/src/controllers/group_controller.js
@@ -3,6 +3,7 @@ import {
     getMembers,
     createGroup,
     joinGroupViaCode,
+    getGroupInformationViaCode,
 } from "../models/group_model.js";
 import User from "../models/user_model.js";
 const getGroupInformation = async (req, res) => {
@@ -37,7 +38,26 @@ const createNewGroup = async (req, res) => {
 
 const getPublicInformation = async (req, res) => {
     const slug = req.params.slug;
-    return res.status(200).json({ msg: "getPublicInformation" });
+    const { invitation_code } = req.query;
+    const group = await getGroupInformationViaCode(slug, invitation_code);
+    if (group.error) {
+        return res.status(400).json({ error: group.error });
+    }
+    const groupUsers = await getMembers(group.id);
+    if (groupUsers.length === 0) {
+        return res
+            .status(400)
+            .json({ error: "Request Error: Group not exist." });
+    }
+    const userIds = groupUsers.map((user) => user.user_id);
+    const requirement = { uid: userIds };
+    const memberUsers = await User.getUsers(requirement);
+    if (memberUsers.length === 0) {
+        return res
+            .status(500)
+            .json({ error: "Internal Server Error: User not exist." });
+    }
+    return res.status(200).json({ group: group, members: memberUsers });
 };
 
 const joinGroup = async (req, res) => {
