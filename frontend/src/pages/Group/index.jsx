f4c11e51c6f31120a6ad144450bc1eec195fea36
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-17 18:10:29 +0800

    [Group] add logic for display Group page

--- frontend/src/pages/Group/index.jsx
+++ frontend/src/pages/Group/index.jsx
@@ -1,12 +1,10 @@
 import GroupDashboard from "./components/Dashboard";
 import Tabs from "./components/Tabs";
 import Error from "./components/Error";
-import { GroupContextProvider } from "../../contexts/GroupContext";
 import { ExpenseContextProvider } from "../../contexts/ExpenseContext";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import { useContext } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
 import { GroupContext } from "../../contexts/GroupContext";
 
 const WrapperGroupContainer = styled.div`
@@ -16,29 +14,22 @@ const WrapperGroupContainer = styled.div`
 `;
 
 const Group = () => {
-    const { isLogin } = useContext(AuthContext);
     const { group } = useContext(GroupContext);
-    console.log("isLogin", isLogin);
-    const { loading } = useContext(AuthContext);
-    console.log("isLogin", isLogin);
-    console.log("loading", loading);
-    console.log("group", group);
+
     return (
-        <GroupContextProvider>
-            <ExpenseContextProvider>
-                {isLogin || group.id ? (
-                    <WrapperGroupContainer>
-                        <GroupDashboard></GroupDashboard>
-                        <Tabs></Tabs>
-                        <Outlet></Outlet>
-                    </WrapperGroupContainer>
-                ) : (
-                    <WrapperGroupContainer>
-                        <Error></Error>
-                    </WrapperGroupContainer>
-                )}
-            </ExpenseContextProvider>
-        </GroupContextProvider>
+        <ExpenseContextProvider>
+            {Object.keys(group).length !== 0 ? (
+                <WrapperGroupContainer>
+                    <GroupDashboard></GroupDashboard>
+                    <Tabs></Tabs>
+                    <Outlet></Outlet>
+                </WrapperGroupContainer>
+            ) : (
+                <WrapperGroupContainer>
+                    <Error></Error>
+                </WrapperGroupContainer>
+            )}
+        </ExpenseContextProvider>
     );
 };
 
