540103df64a646d75cbedd9181623fc5f31bc649
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-19 00:14:30 +0800

    [optimize] add key for react

--- frontend/src/pages/Group/pages/Debts/components/DebtsBlock.jsx
+++ frontend/src/pages/Group/pages/Debts/components/DebtsBlock.jsx
@@ -22,65 +22,67 @@ const DebtsBlock = () => {
                 bgcolor: "background.paper",
             }}
         >
-            {Object.entries(debts).map(([currencyOption, transactions]) => {
-                const [currency] = CURRENCY_OPTIONS.filter(
-                    (currency) => currency.id === Number(currencyOption)
-                );
-                return transactions.map((transaction, index) => {
-                    const labelId = `checkbox-list-secondary-label-${index}`;
-                    const debtor = members[transaction[0]];
-                    const creditor = members[transaction[1]];
-                    const debtAmounts = transaction[2];
-
-                    return (
-                        <>
-                            <Divider></Divider>
-                            <ListItem key={index}>
-                                <ListItemButton>
-                                    <ListItemAvatar>
-                                        <Avatar
-                                            alt={`${debtor.name}`}
-                                            src={debtor.image}
-                                            sx={{ width: 50, height: 50 }}
-                                        />
-                                    </ListItemAvatar>
-                                    <Container>
-                                        <ListItemText
-                                            primary={debtor.name}
-                                            secondary={`${
-                                                currency.symbol
-                                            } ${Number(
-                                                debtAmounts.toFixed(2)
-                                            )}`}
-                                        />
-                                    </Container>
-                                    <Container>
-                                        <ListItemText
-                                            id={labelId}
-                                            primary="→"
-                                            sx={{ textAlign: "center" }}
-                                        />
-                                    </Container>
-                                    <Container>
-                                        <ListItemText
-                                            primary={creditor.name}
-                                            sx={{ textAlign: "right" }}
-                                        />
-                                    </Container>
-
-                                    <ListItemAvatar>
-                                        <Avatar
-                                            alt={creditor.name}
-                                            src={creditor.image}
-                                            sx={{ width: 50, height: 50 }}
-                                        />
-                                    </ListItemAvatar>
-                                </ListItemButton>
-                            </ListItem>
-                        </>
+            {Object.entries(debts).map(
+                ([currencyOption, transactions], index) => {
+                    const [currency] = CURRENCY_OPTIONS.filter(
+                        (currency) => currency.id === Number(currencyOption)
                     );
-                });
-            })}
+                    return transactions.map((transaction, index) => {
+                        const labelId = `checkbox-list-secondary-label-${index}`;
+                        const debtor = members[transaction[0]];
+                        const creditor = members[transaction[1]];
+                        const debtAmounts = transaction[2];
+
+                        return (
+                            <div key={"debtorBlockList" + index}>
+                                <Divider></Divider>
+                                <ListItem>
+                                    <ListItemButton>
+                                        <ListItemAvatar>
+                                            <Avatar
+                                                alt={`${debtor.name}`}
+                                                src={debtor.image}
+                                                sx={{ width: 50, height: 50 }}
+                                            />
+                                        </ListItemAvatar>
+                                        <Container>
+                                            <ListItemText
+                                                primary={debtor.name}
+                                                secondary={`${
+                                                    currency.symbol
+                                                } ${Number(
+                                                    debtAmounts.toFixed(2)
+                                                )}`}
+                                            />
+                                        </Container>
+                                        <Container>
+                                            <ListItemText
+                                                id={labelId}
+                                                primary="→"
+                                                sx={{ textAlign: "center" }}
+                                            />
+                                        </Container>
+                                        <Container>
+                                            <ListItemText
+                                                primary={creditor.name}
+                                                sx={{ textAlign: "right" }}
+                                            />
+                                        </Container>
+
+                                        <ListItemAvatar>
+                                            <Avatar
+                                                alt={creditor.name}
+                                                src={creditor.image}
+                                                sx={{ width: 50, height: 50 }}
+                                            />
+                                        </ListItemAvatar>
+                                    </ListItemButton>
+                                </ListItem>
+                            </div>
+                        );
+                    });
+                }
+            )}
         </List>
     );
 };
