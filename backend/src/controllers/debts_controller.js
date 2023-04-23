646016279344b29ed23b356b50fbbc096eee9150
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-23 17:54:01 +0800

    [controller] modify getSettlingGroupDebts to calculate settlement

--- backend/src/controllers/debts_controller.js
+++ backend/src/controllers/debts_controller.js
@@ -1,10 +1,14 @@
-import { getMembers } from "../models/group_model.js";
+import { getMembers, getMember } from "../models/group_model.js";
 import {
     getExpensesByGroupId,
     getSettlingExpensesByGroupId,
 } from "../models/expense_model.js";
-import { minimizeDebts } from "../models/debts_model.js";
-const getGroupDebts = async (req, res, next) => {
+import {
+    createSettlement,
+    getSettlementsByGroupId,
+} from "../models/debts_model.js";
+import { minimizeDebts } from "../models/split_model.js";
+const getGroupDebts = async (req, res) => {
     const group_id = req.params.group_id;
     const groupMembers = await getMembers(group_id);
     const membersIndexMap = new Map();
@@ -65,7 +69,7 @@ const getGroupDebts = async (req, res, next) => {
     return res.status(200).json(currencyTransactions);
 };
 
-const getSettlingGroupDebts = async (req, res, next) => {
+const getSettlingGroupDebts = async (req, res) => {
     const group_id = req.params.group_id;
     const groupMembers = await getMembers(group_id);
     const membersIndexMap = new Map();
@@ -77,6 +81,9 @@ const getSettlingGroupDebts = async (req, res, next) => {
     const currencyTransactions = {};
 
     const groupExpenses = await getSettlingExpensesByGroupId(group_id);
+    const settlement = await getSettlementsByGroupId(group_id);
+    // TODO:
+    // console.log(settlement);
 
     groupExpenses.forEach((expense) => {
         if (expense.currency_option in currencyGraph === false) {
@@ -119,11 +126,66 @@ const getSettlingGroupDebts = async (req, res, next) => {
             }
         }
     });
+
+    const settlementTransactions = {};
+    // TODO:
+    // console.log(membersIndexMap);
+    settlement.forEach((settlement) => {
+        if (settlement.currency_option in settlementTransactions === false) {
+            settlementTransactions[settlement.currency_option] = [];
+        }
+        const payerIndex = membersIndexMap.get(settlement.payer_id);
+        const payeeIndex = membersIndexMap.get(settlement.payee_id);
+        settlementTransactions[settlement.currency_option].push([
+            payerIndex,
+            payeeIndex,
+            settlement.amount,
+        ]);
+    });
+    // TODO:
+    // console.log(currencyGraph);
     for (const [currency_option, graph] of Object.entries(currencyGraph)) {
         currencyTransactions[currency_option] = minimizeDebts(graph);
+        for (
+            let i = 0;
+            i < settlementTransactions[currency_option].length;
+            i++
+        ) {
+            const settledDebt = settlementTransactions[currency_option][i];
+            for (
+                let j = 0;
+                j < currencyTransactions[currency_option].length;
+                j++
+            ) {
+                const debt = currencyTransactions[currency_option][j];
+                if (settledDebt.toString() === debt.toString()) {
+                    currencyTransactions[currency_option].splice(j, 1);
+                    break;
+                }
+            }
+        }
     }
 
     return res.status(200).json(currencyTransactions);
 };
 
-export { getGroupDebts, getSettlingGroupDebts };
+const settleUpDebts = async (req, res) => {
+    const user_id = req.user.id;
+    const group_id = req.params.group_id;
+    const groupMember = await getMember(group_id, user_id);
+    if (groupMember.length === 0) {
+        return res.status(400).json({
+            error: "Unauthorized: Only group members can settle the debts.",
+        });
+    }
+    const settlementData = req.body;
+    settlementData.group_id = group_id;
+    const settlementResult = await createSettlement(settlementData);
+    if (settlementResult === -1) {
+        return res.status(500).json({ error: "Internal Server Error: MySQL" });
+    }
+
+    return res.status(200).json("hello");
+};
+
+export { getGroupDebts, getSettlingGroupDebts, settleUpDebts };
