645afa83fe8abf6e77d40050c0df6e6c09ef155b
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-23 17:51:35 +0800

    [model] add function to create settlement

---
+++ backend/src/models/debts_model.js
@@ -0,0 +1,39 @@
+import { pool } from "../databases/MySQL.database.js";
+import dotenv from "dotenv";
+import path from "path";
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+dotenv.config({ path: __dirname + "/../../.env" });
+
+const createSettlement = async (settlementData) => {
+    const connection = await pool.getConnection();
+    try {
+        await connection.query("START TRANSACTION");
+        await connection.query(
+            "INSERT INTO `settlements` SET ?",
+            settlementData
+        );
+        await connection.query("COMMIT");
+        return 0;
+    } catch (error) {
+        console.error(error);
+        await connection.query("ROLLBACK");
+        return -1;
+    } finally {
+        await connection.release();
+    }
+};
+
+const getSettlementsByGroupId = async (group_id) => {
+    try {
+        const [settlements] = await pool.query(
+            "SELECT * FROM `settlements` WHERE group_id = ?",
+            [group_id]
+        );
+        return settlements;
+    } catch (error) {
+        console.error(error);
+        return { error };
+    }
+};
+
+export { createSettlement, getSettlementsByGroupId };
