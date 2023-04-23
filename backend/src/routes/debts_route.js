977f019787678734708d8e3717c97c643b27e403
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-23 17:54:15 +0800

    [route] add authentication

--- backend/src/routes/debts_route.js
+++ backend/src/routes/debts_route.js
@@ -1,13 +1,18 @@
 import express from "express";
 const router = express.Router();
-import { wrapAsync } from "../utils/util.js";
+import { wrapAsync, authentication } from "../utils/util.js";
+
 import {
     getGroupDebts,
     getSettlingGroupDebts,
+    settleUpDebts,
 } from "../controllers/debts_controller.js";
 
 /* GET debts data */
-router.route("/debts/:group_id").get(wrapAsync(getGroupDebts));
+router
+    .route("/debts/:group_id")
+    .get(authentication(), wrapAsync(getGroupDebts))
+    .post(authentication(), wrapAsync(settleUpDebts));
 
 router.route("/debts/:group_id/settling").get(wrapAsync(getSettlingGroupDebts));
 
