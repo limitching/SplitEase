dabbe4ef74670a06ac914d7cb1562f83249d29e6
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-23 14:42:17 +0800

    [route] add route for getting settling expense

--- backend/src/routes/debts_route.js
+++ backend/src/routes/debts_route.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
 import { wrapAsync } from "../utils/util.js";
-import { getGroupDebts } from "../controllers/debts_controller.js";
+import {
+    getGroupDebts,
+    getSettlingGroupDebts,
+} from "../controllers/debts_controller.js";
 
 /* GET debts data */
 router.route("/debts/:group_id").get(wrapAsync(getGroupDebts));
+
+router.route("/debts/:group_id/settling").get(wrapAsync(getSettlingGroupDebts));
 
 export default router;
