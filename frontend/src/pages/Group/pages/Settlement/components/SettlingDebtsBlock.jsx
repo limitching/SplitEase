df1249367fd4d1333dfc8ae61f2e779a6dd07f55
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-23 18:15:25 +0800

    [Group] add SettlingDebtsBlock component

---
+++ frontend/src/pages/Group/pages/Settlement/components/SettlingDebtsBlock.jsx
@@ -0,0 +1,90 @@
+import { CURRENCY_OPTIONS } from "../../../../../global/constant";
+import { useContext } from "react";
+import { GroupContext } from "../../../../../contexts/GroupContext";
+import {
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    ListItemAvatar,
+    Avatar,
+    Divider,
+} from "@mui/material";
+import { Container } from "react-bootstrap";
+const SettlingDebtsBlock = () => {
+    const { members, settlingDebts, debts } = useContext(GroupContext);
+    return (
+        <List
+            dense
+            sx={{
+                width: "100%",
+                maxWidth: "100%",
+                bgcolor: "background.paper",
+            }}
+        >
+            {Object.entries(settlingDebts).map(
+                ([currencyOption, transactions], index) => {
+                    const [currency] = CURRENCY_OPTIONS.filter(
+                        (currency) => currency.id === Number(currencyOption)
+                    );
+                    return transactions.map((transaction, index) => {
+                        const labelId = `checkbox-list-secondary-label-${index}`;
+                        const debtor = members[transaction[0]];
+                        const creditor = members[transaction[1]];
+                        const debtAmounts = transaction[2];
+
+                        return (
+                            <div key={"debtorBlockList" + index}>
+                                <Divider></Divider>
+                                <ListItem>
+                                    <ListItemButton>
+                                        <ListItemAvatar>
+                                            <Avatar
+                                                alt={`${debtor.name}`}
+                                                src={debtor.image}
+                                                sx={{ width: 50, height: 50 }}
+                                            />
+                                        </ListItemAvatar>
+                                        <Container>
+                                            <ListItemText
+                                                primary={debtor.name}
+                                                secondary={`${
+                                                    currency.symbol
+                                                } ${Number(
+                                                    debtAmounts.toFixed(2)
+                                                )}`}
+                                            />
+                                        </Container>
+                                        <Container>
+                                            <ListItemText
+                                                id={labelId}
+                                                primary="→"
+                                                sx={{ textAlign: "center" }}
+                                            />
+                                        </Container>
+                                        <Container>
+                                            <ListItemText
+                                                primary={creditor.name}
+                                                sx={{ textAlign: "right" }}
+                                            />
+                                        </Container>
+
+                                        <ListItemAvatar>
+                                            <Avatar
+                                                alt={creditor.name}
+                                                src={creditor.image}
+                                                sx={{ width: 50, height: 50 }}
+                                            />
+                                        </ListItemAvatar>
+                                    </ListItemButton>
+                                </ListItem>
+                            </div>
+                        );
+                    });
+                }
+            )}
+        </List>
+    );
+};
+
+export default SettlingDebtsBlock;
