c393938b6e63951a036fe65043d0a6856d593593
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-26 10:17:27 +0800

    [Profile] add ProfileModal

--- frontend/src/components/Header/ProfileButton.jsx
+++ frontend/src/components/Header/ProfileButton.jsx
@@ -1,27 +1,49 @@
 import { Avatar, ListItemButton, ListItemText } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
+import ProfileModal from "./ProfileModal";
 
 const ProfileButton = () => {
     const { user } = useContext(AuthContext);
 
+    const [showProfileModal, setShowProfileModal] = useState(false);
+
+    const handleOpenProfileModal = () => {
+        setShowProfileModal(true);
+    };
+
+    const handleCloseProfileModal = () => {
+        setShowProfileModal(false);
+    };
+
     return (
-        <ListItemButton
-            sx={{
-                height: "3.5rem",
-            }}
-        >
-            <ListItemText
-                id={"profile-button-text"}
-                primary={user.name}
-                secondary={user.email}
+        <>
+            <ListItemButton
                 sx={{
-                    textAlign: "right",
-                    paddingRight: "1rem",
+                    height: "3.5rem",
                 }}
-            />
-            <Avatar alt={user.name} src={user.image} />
-        </ListItemButton>
+                onClick={handleOpenProfileModal}
+            >
+                <ListItemText
+                    id={"profile-button-text"}
+                    primary={user.name}
+                    secondary={user.email}
+                    sx={{
+                        textAlign: "right",
+                        paddingRight: "1rem",
+                    }}
+                />
+                <Avatar
+                    alt={user.name}
+                    src={user.image}
+                    sx={{ border: "1px solid white" }}
+                />
+            </ListItemButton>
+            <ProfileModal
+                showProfileModal={showProfileModal}
+                handleCloseProfileModal={handleCloseProfileModal}
+            ></ProfileModal>
+        </>
     );
 };
 
