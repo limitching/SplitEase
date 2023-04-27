07f2714ca117bcd876ebf62782d22ae8fbf0f546
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-27 19:44:31 +0800

    [refactor] add createCurrencyGraph

--- backend/src/models/debts_model.js
+++ backend/src/models/debts_model.js
@@ -63,4 +63,59 @@ const getSettlingByGroupId = async (group_id) => {
     }
 };
 
-export { createSettlement, getSettlementsByGroupId, getSettlingByGroupId };
+const createCurrencyGraph = async (
+    groupMembers,
+    membersIndexMap,
+    groupExpenses,
+    currencyGraph,
+    currencyTransactions
+) => {
+    groupExpenses.forEach((expense) => {
+        if (expense.currency_option in currencyGraph === false) {
+            const debtsGraph = new Array(groupMembers.length);
+            for (let i = 0; i < debtsGraph.length; i++) {
+                debtsGraph[i] = new Array(groupMembers.length).fill(0);
+            }
+            currencyGraph[expense.currency_option] = debtsGraph;
+            currencyTransactions[expense.currency_option] = null;
+        }
+        const weights = Array.from(expense.debtors_weight.values());
+        const totalWeight = weights.reduce((acc, curr) => acc + curr, 0);
+        const adjustments = Array.from(expense.debtors_adjustment.values());
+        const totalAdjustments = adjustments.reduce(
+            (acc, curr) => acc + curr,
+            0
+        );
+
+        for (let [creditor, creditorAmount] of expense.creditors_amounts) {
+            for (let [debtor, weight] of expense.debtors_weight) {
+                const creditorIndex = membersIndexMap.get(Number(creditor));
+                const debtorIndex = membersIndexMap.get(Number(debtor));
+                const proportionAdjustAmount =
+                    (totalAdjustments * creditorAmount) / expense.amount;
+                const debtorAdjustAmount = expense.debtors_adjustment.has(
+                    debtor
+                )
+                    ? expense.debtors_adjustment.get(debtor)
+                    : 0;
+
+                // Only calculate case: creditorIndex !== debtorIndex
+                if (creditorIndex !== debtorIndex) {
+                    currencyGraph[expense.currency_option][creditorIndex][
+                        debtorIndex
+                    ] +=
+                        ((creditorAmount - proportionAdjustAmount) * weight) /
+                            totalWeight +
+                        debtorAdjustAmount;
+                }
+            }
+        }
+    });
+};
+
+export {
+    createSettlement,
+    getSettlementsByGroupId,
+    getSettlingByGroupId,
+    createCurrencyGraph,
+};
