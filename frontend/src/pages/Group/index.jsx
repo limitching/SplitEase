3f9eeedbb671c065f77115c22bce02bbc1f52d76
Hung Wei-Ching <hwc0320@gmail.com>
2023-04-29 14:25:50 +0800

    [Group] add invitation page logic to group index

--- frontend/src/pages/Group/index.jsx
+++ frontend/src/pages/Group/index.jsx
@@ -9,6 +9,7 @@ import { GroupContext } from "../../contexts/GroupContext";
 import { AuthContext } from "../../contexts/AuthContext";
 import Loading from "../../components/Loading";
 import { GROUP_BG_COLOR } from "../../global/constant";
+import Invitation from "./components/Invitation";
 
 const WrapperGroupContainer = styled.div`
     padding-top: 55px;
@@ -18,7 +19,7 @@ const WrapperGroupContainer = styled.div`
 `;
 
 const Group = () => {
-    const { group, isLoading } = useContext(GroupContext);
+    const { group, isLoading, isPublicVisit } = useContext(GroupContext);
     const { loading } = useContext(AuthContext);
     if (isLoading || loading) {
         return <Loading />;
@@ -27,11 +28,17 @@ const Group = () => {
     return (
         <ExpenseContextProvider>
             {Object.keys(group).length !== 0 ? (
-                <WrapperGroupContainer>
-                    <GroupDashboard></GroupDashboard>
-                    <Tabs></Tabs>
-                    <Outlet></Outlet>
-                </WrapperGroupContainer>
+                isPublicVisit ? (
+                    <WrapperGroupContainer>
+                        <Invitation></Invitation>
+                    </WrapperGroupContainer>
+                ) : (
+                    <WrapperGroupContainer>
+                        <GroupDashboard></GroupDashboard>
+                        <Tabs></Tabs>
+                        <Outlet></Outlet>
+                    </WrapperGroupContainer>
+                )
             ) : (
                 <WrapperGroupContainer>
                     <Error></Error>
