a0b137fe0ff963bba6555e132e96182715755c4a
Hung Wei-Ching <hwc0320@gmail.com>
2023-05-07 15:26:14 +0800

    [logs] remove unused logs

--- frontend/src/pages/Index/components/Hero/Hero.jsx
+++ frontend/src/pages/Index/components/Hero/Hero.jsx
@@ -48,7 +48,7 @@ function Hero() {
     const navigate = useNavigate();
 
     const particlesInit = useCallback(async (engine) => {
-        console.log(engine);
+        // console.log(engine);
         // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
         // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
         // starting from v2 you can add only the features you need reducing the bundle size
@@ -56,7 +56,7 @@ function Hero() {
     }, []);
 
     const particlesLoaded = useCallback(async (container) => {
-        await console.log(container);
+        // await console.log(container);
     }, []);
 
     const particlesParams = {
