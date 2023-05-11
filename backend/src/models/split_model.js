3279e41259f1ce0cbecae268e43503358c5c970c
HUNG WEI-CHING <hwc0320@gmail.com>
2023-05-11 11:13:21 +0800

    [refactor] refactor buildResidualGraph and buildLevelGraph

--- backend/src/models/split_model.js
+++ backend/src/models/split_model.js
@@ -1,41 +1,44 @@
-// A function to build residual graph
+// Creates a deep copy of the input graph, returning a new 2D array with the same elements
 function buildResidualGraph(graph) {
-    const N = graph.length;
-    const residualGraph = new Array(N);
+    const nodeCount = graph.length;
+    const residualGraph = new Array(nodeCount);
 
-    // Deep copy current graph
-    for (let i = 0; i < N; i++) {
-        residualGraph[i] = new Array(N).fill(0);
-        for (let j = 0; j < N; j++) {
-            residualGraph[i][j] = graph[i][j];
+    // Create new array and copy elements
+    for (let i = 0; i < nodeCount; i++) {
+        residualGraph[i] = new Array(nodeCount).fill(0); // Create new row in residual graph
+        for (let j = 0; j < nodeCount; j++) {
+            residualGraph[i][j] = graph[i][j]; // Copy element from input graph to residual graph
         }
     }
+
     return residualGraph;
 }
 
-// A function to build levelGraph
+// Build level graph using BFS traversal on residual graph
 function buildLevelGraph(residualGraph, start, end) {
-    const N = residualGraph.length;
-    const levelGraph = new Array(N).fill(-1);
-    const queue = [];
-    queue.push(start);
-    levelGraph[start] = 0;
+    const nodeCount = residualGraph.length;
+    const levelGraph = new Array(nodeCount).fill(-1);
 
-    while (queue.length > 0) {
-        const u = queue.shift();
-        for (let v = 0; v < N; v++) {
-            if (residualGraph[u][v] > 0 && levelGraph[v] === -1) {
-                levelGraph[v] = levelGraph[u] + 1;
-                queue.push(v);
+    // Set starting node's level to 0 and add to queue
+    levelGraph[start] = 0;
+    const queue = [start];
+
+    // BFS traversal on residual graph
+    while (queue.length) {
+        const current = queue.shift();
+        const neighbors = residualGraph[current];
+        for (let neighbor = 0; neighbor < nodeCount; neighbor++) {
+            const isPathExist = neighbors[neighbor] > 0;
+            const isNeighborNotVisited = levelGraph[neighbor] === -1;
+            if (isPathExist && isNeighborNotVisited) {
+                levelGraph[neighbor] = levelGraph[current] + 1;
+                queue.push(neighbor);
+                if (neighbor === end) return levelGraph; // Early return if end node is reached
             }
         }
     }
 
-    if (levelGraph[end] === -1) {
-        return null;
-    } else {
-        return levelGraph;
-    }
+    return null; // No path from start to end
 }
 
 // A function to find BlockingFlow
