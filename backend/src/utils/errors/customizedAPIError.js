3b60cb259f5d4aa1082ec9b1f9013a9d144d1c14
HUNG WEI-CHING <hwc0320@gmail.com>
2023-05-12 17:01:03 +0800

    [error] rename customizedAPIError, add static method

--- backend/src/utils/errors/customizedAPIError.js
+++
@@ -1,8 +0,0 @@
-class customizedError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
-export default customizedError;
---
+++ backend/src/utils/errors/customizedError.js
@@ -0,0 +1,28 @@
+class customizedError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode || 500;
+    Error.captureStackTrace(this, this.constructor);
+  }
+  static badRequest(message) {
+    return new customizedError(message || "Bad Request", 400);
+  }
+  static unauthorized(message) {
+    return new customizedError(message || "Unauthorized", 401);
+  }
+  static forbidden(message) {
+    return new customizedError(message || "Forbidden", 403);
+  }
+  static notFound(message) {
+    return new customizedError(message || "Not Found", 404);
+  }
+  static tooManyRequests(message) {
+    return new customizedError(message || "Too Many Requests", 429);
+  }
+  static internal(message) {
+    return new customizedError(message || "Internal Server Error", 500);
+  }
+}
+
+export { customizedError };
