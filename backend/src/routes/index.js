66798c7034329b58cb4ab06e442ffa00cd7cd38d
HUNG WEI-CHING <hwc0320@gmail.com>
2023-05-12 14:00:58 +0800

    [route]index route modify comments

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,21 +3,21 @@ import generateUploadUrl from "../services/S3.js";
 import { wrapAsync, authentication } from "../utils/util.js";
 const router = express.Router();
 
-/* Health check page for AWS ELB. */
+/* Health check page for AWS Application Load Balancer. */
 router.get("/", function (req, res) {
-    return res.status(200);
+  return res.status(200);
 });
 
 router.route("/s3url").get(
-    authentication(),
-    wrapAsync(async (req, res) => {
-        try {
-            const { url, imageName } = await generateUploadUrl();
-            return res.status(200).json({ url, imageName });
-        } catch (error) {
-            return res.status(500).json({ error });
-        }
-    })
+  authentication(),
+  wrapAsync(async (req, res) => {
+    try {
+      const { url, imageName } = await generateUploadUrl();
+      return res.status(200).json({ url, imageName });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  })
 );
 
 export default router;
