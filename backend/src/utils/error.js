4333044e67282cdc6991bcf8cca3aaad16934657
HUNG WEI-CHING <hwc0320@gmail.com>
2023-05-12 18:12:31 +0800

    [error] modify error.js

--- backend/src/utils/error.js
+++ backend/src/utils/error.js
@@ -1,12 +1,12 @@
 class customizedError extends Error {
   constructor(message, statusCode) {
     super(message);
-    this.name = this.constructor.name;
+    this.message = message || "Internal Server Error";
     this.statusCode = statusCode || 500;
     Error.captureStackTrace(this, this.constructor);
   }
   static badRequest(message) {
-    return new customizedError(message || "Bad Request", 400);
+    return new customizedError(message ? message : "Bad Request", 400);
   }
   static unauthorized(message) {
     return new customizedError(message || "Unauthorized", 401);
