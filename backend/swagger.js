2b81c233ad40888d5e39a3d05b9e38d6eb8bfed3
HUNG WEI-CHING <hwc0320@gmail.com>
2023-05-16 22:22:49 +0800

    [swagger] modify swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -3,14 +3,13 @@ import path from "path";
 import dotenv from "dotenv";
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 dotenv.config({ path: __dirname + "/.env" });
-const { API_VERSION } = process.env;
 
 const outputFile = "./swagger-output.json";
 const endpointsFiles = ["./app.js"];
 
 const doc = {
   info: {
-    version: API_VERSION,
+    version: 1.0,
     title: "SplitEase API",
     description: "An API documentation for SplitEase",
   },
