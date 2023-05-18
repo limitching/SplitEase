dbfaf1cebbd40dcff9c8f76febc8f89de6081003
HUNG WEI-CHING <hwc0320@gmail.com>
2023-05-18 14:43:51 +0800

    [Footer] add link for swagger documentation

--- frontend/src/pages/Index/components/FooterBlock/FooterBlock.jsx
+++ frontend/src/pages/Index/components/FooterBlock/FooterBlock.jsx
@@ -6,110 +6,78 @@ import Tooltip from "@mui/material/Tooltip";
 import styled from "styled-components";
 
 const FooterText = styled.h4`
-    color: "black";
+  color: "black";
 `;
 
 function FooterBlock() {
-    return (
-        <>
-            <footer id="footer" className="footer-area pt-200">
-                <div className="container">
-                    <div className="footer-widget pb-100">
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6 col-sm-8">
-                                <div
-                                    className="footer-about mt-200 wow fadeIn"
-                                    data-wow-duration="1s"
-                                    data-wow-delay="0.2s"
-                                >
-                                    <a className="logo" href="#">
-                                        <img
-                                            src="assets/images/logo.svg"
-                                            alt="logo"
-                                        />
-                                    </a>
+  return (
+    <>
+      <footer id="footer" className="footer-area pt-200">
+        <div className="container">
+          <div className="footer-widget pb-100">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-8">
+                <div className="footer-about mt-200 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+                  <a className="logo" href="#">
+                    <img src="assets/images/logo.svg" alt="logo" />
+                  </a>
 
-                                    <FooterText>
-                                        <br></br>
-                                        Your bill splitting assistant
-                                    </FooterText>
-                                    <div style={{ marginTop: "2rem" }}>
-                                        <Tooltip title="LinedIn">
-                                            <a
-                                                href="https://www.linkedin.com/in/limitching/"
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <IconButton
-                                                    color="secondary"
-                                                    aria-label="add an alarm"
-                                                >
-                                                    <img
-                                                        src="/assets/images/linkedin.png"
-                                                        alt="Linkedin icon"
-                                                        style={{
-                                                            width: "30px",
-                                                        }}
-                                                    ></img>
-                                                </IconButton>
-                                            </a>
-                                        </Tooltip>
-                                        <Tooltip title="Github">
-                                            <a
-                                                href="https://github.com/limitching"
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <IconButton
-                                                    color="secondary"
-                                                    aria-label="add an alarm"
-                                                >
-                                                    <img
-                                                        src="/assets/images/github.png"
-                                                        alt="Github icon"
-                                                        style={{
-                                                            width: "30px",
-                                                        }}
-                                                    ></img>
-                                                </IconButton>
-                                            </a>
-                                        </Tooltip>
-                                        <Tooltip title="Swagger">
-                                            <a
-                                                href="#"
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <IconButton
-                                                    color="secondary"
-                                                    aria-label="add an alarm"
-                                                >
-                                                    <img
-                                                        src="/assets/images/swagger.png"
-                                                        alt="Swagger"
-                                                        style={{
-                                                            width: "30px",
-                                                        }}
-                                                    ></img>
-                                                </IconButton>
-                                            </a>
-                                        </Tooltip>
-                                    </div>
-                                    <p
-                                        className="text"
-                                        style={{ color: "black" }}
-                                    >
-                                        Copyright © 2023 SplitEase. All rights
-                                        reserved.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  <FooterText>
+                    <br></br>
+                    Your bill splitting assistant
+                  </FooterText>
+                  <div style={{ marginTop: "2rem" }}>
+                    <Tooltip title="LinedIn">
+                      <a href="https://www.linkedin.com/in/limitching/" target="_blank" rel="noopener noreferrer">
+                        <IconButton color="secondary" aria-label="add an alarm">
+                          <img
+                            src="/assets/images/linkedin.png"
+                            alt="Linkedin icon"
+                            style={{
+                              width: "30px"
+                            }}
+                          ></img>
+                        </IconButton>
+                      </a>
+                    </Tooltip>
+                    <Tooltip title="Github">
+                      <a href="https://github.com/limitching" target="_blank" rel="noopener noreferrer">
+                        <IconButton color="secondary" aria-label="add an alarm">
+                          <img
+                            src="/assets/images/github.png"
+                            alt="Github icon"
+                            style={{
+                              width: "30px"
+                            }}
+                          ></img>
+                        </IconButton>
+                      </a>
+                    </Tooltip>
+                    <Tooltip title="Swagger">
+                      <a href="https://api.splitease.cc/api-docs/" target="_blank" rel="noopener noreferrer">
+                        <IconButton color="secondary" aria-label="add an alarm">
+                          <img
+                            src="/assets/images/swagger.png"
+                            alt="Swagger"
+                            style={{
+                              width: "30px"
+                            }}
+                          ></img>
+                        </IconButton>
+                      </a>
+                    </Tooltip>
+                  </div>
+                  <p className="text" style={{ color: "black" }}>
+                    Copyright © 2023 SplitEase. All rights reserved.
+                  </p>
                 </div>
-            </footer>
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default FooterBlock;
